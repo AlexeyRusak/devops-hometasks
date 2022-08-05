15c2a8026ce8425d50f83ff069a6e72ae6a505af
Артем Семин <a.semin@softmax.by>
2022-08-05 15:13:58 +0300

    PHP is working

--- task-01/Parametr.sh
+++ task-01/Parametr.sh
@@ -2,9 +2,14 @@ sudo apt update
 sudo apt -y install php
 sudo apt -y install libapache2-mod-php
 sudo systemctl restart apache2
-echo "<?php phpinfo(); ?>" > /var/www/html/info.php
+echo 'Listen 81' >> /etc/apache2/ports.conf
+sudo mkdir -p /var/www/php
+echo "<?php phpinfo(); ?>" > /var/www/php/info.php
+echo "<VirtualHost *:81>
+        ServerAdmin webmaster@localhost
+        DocumentRoot /var/www/php/info.php
+        ErrorLog ${APACHE_LOG_DIR}/error.log
+        CustomLog ${APACHE_LOG_DIR}/access.log combined
+</VirtualHost>" > /etc/apache2/sites-available/000-default.conf
+
 sudo systemctl restart apache2
-
-
-
-
