517cc4e4d0d3ea0c4bc094f151a5297f767c4d1b
AlexeyRusak <alexey_rusak@mail.ru>
2022-08-06 14:24:16 +0300

    PHP Centos8 - finished

--- task -01.1/ParametrCentos.sh
+++ task -01.1/ParametrCentos.sh
@@ -6,11 +6,27 @@ sudo dnf module install php:remi-8.0
 sudo dnf -y install php
 sudo dnf -y install mysql-server
 sudo -i
-echo "<?php phpinfo(); ?>" >> /var/www/html/info.php
+sudo mkdir -p /var/www/php
+echo "<?php phpinfo(); ?>" >> /var/www/php/index.php
 firewall-cmd --zone=public --add-port=81/tcp
 echo "
 IncludeOptional conf.d/*.conf
+IncludeOptional sites-enabled/*.conf
 Listen 81
 "  >> /etc/httpd/conf/httpd.conf
 
 sudo systemctl restart httpd
+sudo -i
+sudo mkdir /etc/httpd/sites-available /etc/httpd/sites-enabled
+echo "
+<VirtualHost *:80>
+  DocumentRoot "/var/www/html"
+</VirtualHost>
+<VirtualHost *:81>
+  DocumentRoot "/var/www/php/index.php"
+</VirtualHost>
+"  >> /etc/httpd/sites-available/php.conf
+sudo ln -s /etc/httpd/sites-available/php.conf /etc/httpd/sites-enabled/php.conf
+sudo setsebool -P httpd_unified 1
+sudo systemctl restart httpd
+
