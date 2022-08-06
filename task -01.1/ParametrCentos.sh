86bd085c13dad204cb47ee15326bc7bb90145015
AlexeyRusak <alexey_rusak@mail.ru>
2022-08-06 12:25:50 +0300

    testing

--- task -01.1/ParametrCentos.sh
+++ task -01.1/ParametrCentos.sh
@@ -5,12 +5,11 @@ sudo dnf module reset php
 sudo dnf module install php:remi-8.0
 sudo dnf -y install php
 sudo dnf -y install mysql-server
-sudo dnf install php-{common,mysql,xml,xmlrpc,curl,gd,imagick,cli,fpm,mbstring,opcache,zip}
 sudo -i
 echo "<?php phpinfo(); ?>" >> /var/www/html/info.php
 firewall-cmd --zone=public --add-port=81/tcp
 echo "
-IncludeOptional conf.d/php.conf
+IncludeOptional conf.d/*.conf
 Listen 81
 "  >> /etc/httpd/conf/httpd.conf
 
